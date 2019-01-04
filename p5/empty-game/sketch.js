339e5a5f07dea3918089b80e48109ffa16baabd9
swood049 <38500060+swood049@users.noreply.github.com>
2019-01-04 16:57:18 -0500

    Add files via upload

---
+++ p5/empty-game/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // put setup code here
+  createCanvas(640, 480); //size of canvas
+}
+
+function draw() {
+  // put drawing code here
+  background(0,0,15); //this is background
+}
